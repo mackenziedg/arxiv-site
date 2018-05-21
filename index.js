e3c59c49e4973083b987ab59fc962a24901ad324
Mackenzie Gray <mackenzie.gray13@ncf.edu>
2018-05-21 14:11:20 -0400

    Move data to S3.

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var serve = require('express-static');
 var sc = require('skale').context();
-var records = require('./data/records.json');
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+
 var app = express;
 const { body,validationResult } = require('express-validator/check');
 const { sanitizeBody } = require('express-validator/filter');
@@ -16,6 +18,7 @@ app.use(bodyParser.json());
 
 // Routes
 app.get('/', function (req, res){
+
   res.render('index');
 });
 
@@ -29,25 +32,34 @@ calculate_distances_route = [body('arxiv_id', 'Empty url').isLength({min: 1}),
     if (!errors.isEmpty()) {
       res.render('index', {errors: errors});
     } else {
-      var comp_url = 'http://arxiv.org/abs/'+req.body.arxiv_id;
-      var comp_doc = records.filter((d) => d.arxiv_url === comp_url)[0];
 
-      sc.parallelize(records)
-        .map(function(doc, obj){
-          wc1 = doc.word_counts;
-          wc2 = obj.word_counts;
-
-          shared_keys = Object.keys(wc1).filter({}.hasOwnProperty.bind(wc2));;
-          doc.similarity = 0;
-          for(var key in shared_keys){
-            doc.similarity += wc1[shared_keys[key]]*wc2[shared_keys[key]];
-          }
-          doc.similarity = doc.similarity.toFixed(3);
-          return doc;
-        }, comp_doc)
-        .collect().then((r) => res.render('index', {results: r}));
+      var options = {
+        Bucket: process.env.S3_BUCKET,
+        Key: 'records.json'
       }
+
+      s3.getObject(options, function(err, data){
+        var records = JSON.parse(data.Body);
+        var comp_url = 'http://arxiv.org/abs/'+req.body.arxiv_id;
+        var comp_doc = records.filter((d) => d.arxiv_url === comp_url)[0];
+
+        sc.parallelize(records)
+          .map(function(doc, obj){
+            wc1 = doc.word_counts;
+            wc2 = obj.word_counts;
+
+            shared_keys = Object.keys(wc1).filter({}.hasOwnProperty.bind(wc2));;
+            doc.similarity = 0;
+            for(var key in shared_keys){
+              doc.similarity += wc1[shared_keys[key]]*wc2[shared_keys[key]];
+            }
+            doc.similarity = doc.similarity.toFixed(3);
+            return doc;
+          }, comp_doc)
+          .collect().then((r) => res.render('index', {results: r}));
+      });
     }
+  }
 ]
 app.post('/', calculate_distances_route);
 
